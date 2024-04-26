f45be97ce27fa9436241924f4a48538d3acad9f2
JeztC <dagreenrs@gmail.com>
2024-04-26 23:05:35 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -5,36 +5,200 @@
 { config, lib, pkgs, ... }:
 
 {
-	programs.git = {
-		enable = true;
-		config.credential.helper = "libsecret";
-	};
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
-	programs.hyprland = {
-		enable = true;
-		xwayland.enable = true;
-	};
+    programs.hyprland = {
+     enable = true;
+     xwayland.enable = true;
+    };
 
-	security.polkit.enable = true;
-	security.sudo.wheelNeedsPassword = false;
+    programs.git = {
+	enable = true;
+	config.credential.helper = "libsecret";
+    };
 
-	services.xserver.xkb = {
-		layout = "fi";
-	};
-	console.keyMap = "fi";
+  # Use the systemd-boot EFI boot loader.
+  nixpkgs.config.allowUnfree = true;
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "Europe/Vilnius";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
+  services.xserver.xkb = {
+     layout = "fi";
+  };
+
 	xdg.portal = {
 		enable = true;
-		extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
 	};
-	programs.steam = {
+
+  console.keyMap = "fi";
+
+  services.desktopManager.plasma6.enable = true;  
+  services.displayManager.sddm.enable = true;
+  services.xserver.enable = true;
+  security.sudo.wheelNeedsPassword = false;
+
+  users.defaultUserShell = pkgs.nushell;
+
+  	hardware.opengl = {
+		enable = true;
+		driSupport = true;
+		driSupport32Bit = true;
+                extraPackages = with pkgs; [
+                    vaapiVdpau
+                    libvdpau-va-gl
+            ];
+	};
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+
+  security.pam.services.sddm.enableKwallet = true;
+  services.passSecretService.enable = true;
+
+  services.openssh = {
+    enable = true;
+    # require public key authentication for better security
+    settings.PasswordAuthentication = false;
+    settings.KbdInteractiveAuthentication = false;
+    settings.PermitRootLogin = "yes";
+  };
+
+  security.pam.services.login.enableKwallet = true;
+
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.jesse = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" "qemu-libvirtd" "libvirtd" "disk" ]; # Enable ‘sudo’ for the user.
+     packages = with pkgs; [
+       firefox
+       tree
+       discord
+       runelite
+     ];
+   };
+
+ 
+	services.pipewire = {
+		enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
+        pulse.enable = true;
+  };
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     wget
+     htop
+     mullvad-vpn
+     wineWowPackages.waylandFull
+     libreoffice-qt
+     vscode
+     wl-clipboard
+     wl-clip-persist
+     kitty
+     wezterm
+     jetbrains.pycharm-community
+     play-with-mpv
+     tofi
+     cargo
+     teams-for-linux
+     gammastep
+     xwaylandvideobridge
+     grim
+     sqlitebrowser
+     sqlite
+     obs-studio
+     mpv
+     betterbird
+     nwg-look
+     libnotify
+     gcc
+     unrar
+     nodejs
+     insomnia
+     hyprpaper
+     hypridle
+     hyprlock
+     hyprpicker
+     tutanota-desktop
+     gimp
+     unzip
+     eww
+     nushell
+     dunst
+     jq
+     slurp
+     ghc
+     bibata-cursors
+     fuseiso
+     libsForQt5.polkit-kde-agent
+     kdePackages.kolourpaint
+     libsForQt5.kio-extras
+	 libsForQt5.kimageformats
+	 libsForQt5.kdegraphics-thumbnailers
+	 libsForQt5.kservice
+	 kdePackages.kalk
+	 kdePackages.kio-fuse
+	 kdePackages.kpat
+     kdePackages.kirigami
+     libsForQt5.kwallet-pam
+     kdePackages.sonnet
+     python3
+     cmatrix
+     neofetch
+   ];
+ 
+  	programs.steam = {
 		enable = true;
 		remotePlay.openFirewall = true;
 			dedicatedServer.openFirewall = true;
 			package = pkgs.steam.override {
 				extraPkgs = pkgs: with pkgs; [
+						gamescope
 						gamemode
 						mangohud
-                                                gamescope
+						xorg.libXcursor
+						xorg.libXi
+						xorg.libXinerama
+						xorg.libXScrnSaver
 						libpng
 						libpulseaudio
 						libvorbis
@@ -45,262 +209,65 @@
 			};
 	};
 
-	nixpkgs.config.allowUnfree = true;
-	imports =
-		[ 
-		./hardware-configuration.nix
-		];
 
-	nixpkgs.overlays = [
-		(final: prev: {
-		 steam = prev.steam.override ({ extraPkgs ? pkgs': [], ... }: {
-				 extraPkgs = pkgs': (extraPkgs pkgs') ++ (with pkgs'; [
-						 libgdiplus
-				 ]);
-				 });
-		 })
+
+  	fonts.packages = with pkgs; [
+	   vistafonts
+           corefonts
 	];
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
-	networking.networkmanager.enable = true;
-	time.timeZone = "Europe/Vilnius";
+  # List services that you want to enable:
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables.MOZ_ENABLE_WAYLAND = "1";
+  environment.sessionVariables.MOZ_DISABLE_RDD_SANDBOX = "1";
+  environment.sessionVariables.NVD_BACKEND = "direct";
 
-	services.displayManager.sddm.enable = true;
-	services.displayManager.defaultSession = "hyprland";
-	services.xserver.enable = true;
-	services.xserver.videoDrivers = [ "amdgpu" ];
+  services.mullvad-vpn.enable = true;
+  programs.java.enable = true;
+  programs.nix-ld.enable = true;
+  
 
-	services.passSecretService.enable = true;
-	nix.settings.experimental-features = [ "nix-command" "flakes" ]; 
+  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd.qemu.ovmf.enable = true;
+  programs.virt-manager.enable = true;
 
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
 
-	services.printing.enable = true;
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
-	sound.enable = true;
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
 
-	hardware.opengl = {
-		enable = true;
-		driSupport = true;
-		driSupport32Bit = true;
-                extraPackages = with pkgs; [
-                    vaapiVdpau
-                    libvdpau-va-gl
-            ];
-	};
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "23.11"; # Did you read the comment?
 
-	fonts.packages = with pkgs; [
-		font-awesome
-			noto-fonts
-			noto-fonts-cjk
-			noto-fonts-emoji
-			vistafonts
-			corefonts
-			dejavu_fonts
-			fontconfig
-			twemoji-color-font
-			fira-code
-			fira-code-symbols
-			nerdfonts
-			(nerdfonts.override { fonts = [ "RobotoMono"  ]; })
-	];
-
-	fonts.fontconfig.defaultFonts = {
-		monospace = [
-			"terminus"
-				"DejaVu Sans Mono"
-				"IPAGothic"
-		];
-		sansSerif = [
-			"terminus"
-				"DejaVu Sans"
-				"IPAPGothic"
-		];
-		serif = [
-			"terminus"
-				"DejaVu Serif"
-				"IPAPMincho"
-		];
-	};
-
-	services.desktopManager.plasma6.enable = true;
-	#services.xserver.desktopManager.cinnamon.enable = true;
-
-	services.avahi = {
-		enable = true;
-		nssmdns4 = true;
-		openFirewall = true;
-	};
-
-	users.defaultUserShell = pkgs.nushell;
-
-	users.users.jesse = {
-		isNormalUser = true;
-		extraGroups = [ "audio" "keyd" "wheel" "adbusers" "qemu-libvirtd" "libvirtd" "disk"] ;
-			packages = with pkgs; [
-			firefox
-				tree
-				kitty
-				htop
-				discord
-				brave
-			];
-	};
-    
-
-	environment.systemPackages = with pkgs; [
-		    vim
-			wget
-			wezterm
-			neofetch
-			kitty
-			killall
-			wl-clipboard
-			clipman
-			hyprpaper
-			hyprpicker
-			udiskie
-			wl-clip-persist
-			rofi
-			eww
-			libreoffice-qt
-			gammastep
-			hunspell
-			freerdp
-			hunspellDicts.sv_FI
-			xwaylandvideobridge
-			wlogout
-			wleave
-			hunspellDicts.uk_UA
-			jq
-			pkg-config
-			grim
-			sqlitebrowser
-			sqlite
-			slurp
-			obs-studio
-			mpv
-			betterbird
-			bibata-cursors
-			gcc
-			unrar
-			libGL
-			nodejs
-			insomnia
-			vesktop
-			wayland-scanner
-			libsecret
-			tutanota-desktop
-			papirus-icon-theme
-			kdePackages.polkit-kde-agent-1
-			kdePackages.kalk
-			kdePackages.kirigami
-			kdePackages.dolphin
-			kdePackages.sonnet
-			libsForQt5.kio
-                        teams-for-linux
-			play-with-mpv
-			libsForQt5.kio-extras
-			libsForQt5.kimageformats
-			libsForQt5.kdegraphics-thumbnailers
-			libsForQt5.kservice
-			kdePackages.kio-fuse
-			kdePackages.kpat
-                        nixos-bgrt-plymouth
-			wmctrl
-                        yazi
-			libunity
-			dracula-theme
-			dotnet-sdk
-			mono
-			dotnet-runtime
-			nwg-look
-			kdePackages.kate
-			ghc
-			(haskellPackages.ghcWithPackages ( pkgs: with pkgs; [pkgs.raw-strings-qq pkgs.hostname]))
-			hyprlock
-			hyprcursor
-			hypridle
-			haskellPackages.aeson
-			whatsapp-for-linux
-			gimp
-			rofi-power-menu
-			libnotify
-			unzip
-			libvoikko
-			nushell
-			piper
-			cargo
-                        python312Packages.matplotlib
-                        python312Packages.numpy
-                        google-chrome
-                        appimage-run
-                        wineWowPackages.stable
-                        wineWowPackages.waylandFull
-			dunst
-                        vscode
-			jetbrains.idea-community
-			jetbrains.pycharm-community
-                        jetbrains.webstorm
-                        libsForQt5.polkit-kde-agent
-			cmatrix
-			zoom-us
-			exfatprogs
-			virt-manager
-			ntfs3g
-			zellij
-			tofi
-			looking-glass-client
-			ventoy-full
-			ncurses
-			pavucontrol
-			samba
-			fuseiso
-			home-manager
-			mullvad-vpn
-			kdePackages.kolourpaint
-			floorp
-			python3
-			playonlinux
-			stdenv.cc.cc
-			];
-			
-        environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
-	services.mullvad-vpn.enable = true;
-	services.onedrive.enable = true;
-
-	services.samba.enableWinbindd = true;
-
-	programs.java.enable = true;
-	programs.adb.enable = true;
-	programs.nix-ld.enable = true;
-	virtualisation.libvirtd.enable = true;
-	virtualisation.libvirtd.qemu.ovmf.enable = true;
-	programs.virt-manager.enable = true;
-
-	security.pam.services.sddm.enableKwallet = true;
-
-	services.gvfs.enable = true;
-	services.ratbagd.enable = true;
-        
-        services.openssh = {
-  	 enable = true;
-  	 # require public key authentication for better security
-  	 settings.PasswordAuthentication = false;
-  	 settings.KbdInteractiveAuthentication = false;
- 	 settings.PermitRootLogin = "yes";
-	};
-
-	security.rtkit.enable = true;
-	services.pipewire = {
-		enable = true;
-		alsa.enable = true;
-		alsa.support32Bit = true;
-		pulse.enable = true;
-	};
-
-	networking.firewall.enable = false;
-
-	fonts.fontconfig.enable = true;
-
-	system.stateVersion = "23.11"; 
 }
