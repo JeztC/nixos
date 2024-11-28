88158446a8795f2d09f471af75f831ba4bbd9177
JeztC <dagreenrs@gmail.com>
2024-11-28 15:28:54 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -13,11 +13,20 @@
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Use the systemd-boot EFI boot loader.
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
-    
-  # networking.hostName = "nixos"; # Define your hostname.
+boot.loader = {
+  efi = {
+    canTouchEfiVariables = false;
+  };
+    grub = {
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+      device = "nodev";
+    };
+  };
+
+
+  networking.hostName = "matrix";
+
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
@@ -37,49 +46,16 @@
 
   console.keyMap = "fi";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
    users.users.jesse = {
-     initialPassword = "pw123";
      isNormalUser = true;
      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
      packages = with pkgs; [
        tree
        htop
        neofetch
-       kitty
-       tofi
-
+       cmatrix
+       runelite
+       microsoft-edge # Fuck off bro
      ];
    };
 
@@ -133,33 +109,27 @@
 
    programs.nix-ld.enable = true;
    programs.git.enable = true;
-   #programs.firefox = { enable = true; };
-   #programs.java = { enable = true; };
-   #programs.obs-studio.enable = true;
+   programs.firefox.enable = true;
+   programs.java = { enable = true; package = pkgs.jre8; };
+   programs.obs-studio.enable = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+   systemd.services.lactd.enable = true;
+
   environment.systemPackages = with pkgs; [
     vim
     wget
-    vscodium
+    vscodium-fhs
     mpv
     python3
+    libreoffice-qt-fresh
+    thunderbird-latest
     unrar
+    discord
+    godot_4-mono
+    jetbrains.pycharm-community-bin
+    jetbrains.idea-community-bin
+    lact
    ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
 
     security.rtkit.enable = true;
   services.pipewire = {
@@ -167,19 +137,15 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
   };
 
-    # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
 
-    # Modesetting is required.
     modesetting.enable = true;
 
-    powerManagement.enable = false;
+    powerManagement.enable = true;
 
     powerManagement.finegrained = false;
 
@@ -200,44 +166,15 @@
 
   #virtualisation.libvirtd.enable = true;
   #programs.virt-manager.enable = true;
-  #services.mullvad-vpn = { enable = true; package = pkgs.mullvad-vpn; };
+  services.mullvad-vpn = { enable = true; package = pkgs.mullvad-vpn; };
 
-  #environment.sessionVariables.MOZ_ENABLE_WAYLAND = "1";
+  environment.sessionVariables.QT_QPA_PLATFORMTHEME = "kde";
   environment.sessionVariables.__GLX_VENDOR_LIBRARY_NAME = "nvidia";
   environment.sessionVariables.GBM_BACKEND = "nvidia-drm";
   environment.sessionVariables.NVD_BACKEND = "direct";
   environment.sessionVariables.LIBVA_DRIVER_NAME = "nvidia";
   environment.sessionVariables.MOZ_DISABLE_RDD_SANDBOX = "1";
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "24.05";
 
 }
-
