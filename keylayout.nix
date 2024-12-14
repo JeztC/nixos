9be75cb6de0c32e89119dac4884db2519102d031
JeztC <dagreenrs@gmail.com>
2024-12-15 01:41:51 +0200

    remove useless files

--- keylayout.nix
+++
@@ -1,9 +0,0 @@
-{ lib, config, pkgs, ... }:
-
-let
-  compiledLayout = pkgs.runCommand "keyboard-layout" {} ''
-    ${pkgs.xorg.xkbcomp}/bin/xkbcomp ${./path/to/layout.xkb} $out
-  '';
-in
-  services.xserver.displayManager.sessionCommands = "${pkgs.xorg.xkbcomp}/bin/xkbcomp ${compiledLayout} $DISPLAY
-}
